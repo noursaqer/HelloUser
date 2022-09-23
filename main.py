312c2fd8d50e034017c83b6855eb1498059121a2
Noursaqer <nooroned@gmail.com>
2022-09-23 03:01:51 -0700

    assignment of Hello Mr/Mrs

---
+++ main.py
@@ -0,0 +1,17 @@
+Name = input(" name:")
+Age = int(input(" Age:"))
+Address= input(" address:")
+
+if Name.isdigit():
+    print("sorry, please check your info")
+else:
+    print("Hello Mr/Mrs " + Name + " age " + str(Age) + " located in " + Address + "." + " Thanks for participating. Welcome to our community.")
+
+
+
+
+
+
+
+
+
